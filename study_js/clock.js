b43f022252e8733160e16dbba04b440ed1f28647
ldh3907 <ldh165163@gmail.com>
2021-05-10 17:50:11 +0900

    add : clock js
    add : clock js

---
+++ study_js/clock.js
@@ -0,0 +1,16 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector(".js-title");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function init() {
+  getTime();
+}
+
+init();
